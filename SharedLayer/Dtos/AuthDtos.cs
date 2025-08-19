d2cc1128e7ce05347eda6f57f4408b61c883da6f
Volkan <Volkan.05888@gmail.com>
2025-08-19 15:28:19 +0300

    AddedDtos

---
+++ SharedLayer/Dtos/AuthDtos.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedLayer.Dtos
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = string.Empty;
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+        [StringLength(50)]
+        public string? LastName { get; set; }
+        [Phone]
+        public string? PhoneNumber { get; set; }
+    }
+    public class LoginRequest
+    {
+        [Required]
+        public string UsernameOrEmail { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        public bool RememberMe { get; set; }
+    }
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+        public UserDto User { get; set; } = null!;
+
+    }
+    public class RefreshTokenRequest
+    {
+        [Required]
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+    public class UserDto
+    {
+
+        public Guid Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public ICollection<string> Roles { get; set; } = new List<string>();
+    }
+}
---
+++ SharedLayer/Dtos/ProductDtos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLayer.Dtos
+{
+    public class CreateProductRequest
+    {
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(1000)]
+        public string? Description { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int StockQuantity { get; set; }
+    }
+    public class UpdateProductRequest : CreateProductRequest
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+    public class ProductResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+    public class ProductListRequest
+    {
+        [Range(1, int.MaxValue)]
+        public int Page { get; set; } = 1;
+        [Range(1, 100)]
+        public int PageSize { get; set; } = 20;
+        [StringLength(100)]
+        public string? SearchTerm { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal? MinPrice { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal? MaxPrice { get; set; }
+        public string? SortBy { get; set; }
+        public string? SortDirection { get; set; } = "asc";
+
+    }
+    public class ProductListResponse
+    {
+        public List<ProductResponse> Products { get; set; } = new List<ProductResponse>();
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+    }
+    }
